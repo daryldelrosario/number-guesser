e7f0d9b59585da48a4ca5af73478071eab9604f9
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-01-06 07:21:24 -0500

    Generate random number and compare guesses

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+// Return a random integer between 0 - 9
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+}
 
+// Comparing guesses between human and computer
+const compareGuess = (human, computer, target) => {
+    const humanScore = Math.abs(human - target);
+    const computerScore = Math.abs(computer - target);
+    
+    if(humanScore < computerScore) {
+        return true;
+    } else if(humanScore > computerScore) {
+        return false;
+    } else {
+        return true;
+    }
+}
